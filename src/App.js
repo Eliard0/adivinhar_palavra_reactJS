e45d2c77406b1976b7603fa34689426644edc5a6
Eliardo <57414074+Eliard0@users.noreply.github.com>
2022-11-22 14:10:22 -0300

    logica e telas finalizadas falta somente a responsividade

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
 import './App.css';
 import StartScreen from './components/StartScreen';
 
-import { useCallback, useEffect, useState} from "react";
-import {wordsList} from "./data/words";
+import { useCallback, useEffect, useState } from "react";
+import { wordsList } from "./data/words";
 import Game from './components/Game';
 import GameOver from './components/GameOver';
 
 const stages = [
-  { id:1, name: "start" },
-  { id:2, name: "game" },
-  { id:3, name: "end" },
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
 ];
+
+const guessesQty = 4;
 
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
-  
+
   const [pickedWord, setPickedWord] = useState("");
   const [pickedCategory, setPickedCategory] = useState("");
   const [letters, setLetters] = useState([]);
 
-  const pickWordAndCategory = () => {
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(guessesQty);
+  const [score, setScore] = useState(50);
+
+  const pickWordAndCategory = useCallback(() => {
     const categories = Object.keys(words);
     const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
 
     const word = words[category][Math.floor(Math.random() * words[category].length)];
     return { word, category };
-  }
+  }, [words]);
 
-  const startGame = () => {
+  const startGame = useCallback(() => {
+    clearLetterState();
+
     const { word, category } = pickWordAndCategory();
 
     let wordLetter = word.split("");
@@ -37,24 +46,77 @@ function App() {
 
     setPickedWord(word);
     setPickedCategory(category);
-    setLetters(letters);
+    setLetters(wordLetter);
 
     setGameStage(stages[1].name);
+  }, [pickedCategory]);
+
+  const verifyLetter = (letter) => {
+    const normalizedLetter = letter.toLowerCase();
+    if (guessedLetters.includes(normalizedLetter) || wrongLetters.includes(normalizedLetter)) {
+      return;
+    }
+
+    if (letters.includes(normalizedLetter)) {
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        normalizedLetter
+      ])
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+        normalizedLetter,
+      ]);
+
+      setGuesses((actualGuesses) => actualGuesses - 1);
+
+    }
   };
 
-  const verifyLetter = () => {
-    setGameStage(stages[2].name);
+  const clearLetterState = () => {
+    setGuessedLetters([]);
+    setWrongLetters([]);
   }
 
+  useEffect(() => {
+    if (guesses <= 0) {
+      clearLetterState()
+      setGameStage(stages[2].name)
+    }
+  }, [guesses]);
+
+  useEffect(() => {
+
+    const uniqueLetters = [...new Set(letters)];
+
+    if (guessedLetters.length === uniqueLetters.length) {
+      setScore((actualScore) => (actualScore += 100));
+      startGame();
+    }
+  }, [guessedLetters, letters, startGame])
+
   const retry = () => {
-    setGameStage(stages[1].name);
+    setScore(0);
+    setGuesses(guessesQty);
+
+    setGameStage(stages[0].name);
   }
 
   return (
     <div className="App">
-      { gameStage === 'start' && <StartScreen startGame={startGame}/> }
-      { gameStage === 'game' && <Game verifyLetter={verifyLetter}/> }
-      { gameStage === 'end' && <GameOver retry={retry}/> }
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'game' && (
+        <Game
+          verifyLetter={verifyLetter}
+          pickedWord={pickedWord}
+          pickedCategory={pickedCategory}
+          letters={letters}
+          guessedLetters={guessedLetters}
+          wrongLetters={wrongLetters}
+          guesses={guesses}
+          score={score}
+        />)}
+      {gameStage === 'end' && <GameOver retry={retry} score={score} />}
     </div>
   );
 }
